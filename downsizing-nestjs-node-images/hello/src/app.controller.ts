7a24489078aa98df756c2351d48d64af1ba4f873
Your Name <mrsauravsahu@outlook.com>
2021-06-18 01:14:34 +0530

    return app info at root route

--- downsizing-nestjs-node-images/hello/src/app.controller.ts
+++ downsizing-nestjs-node-images/hello/src/app.controller.ts
@@ -1,12 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import * as packageInfo from '../package.json';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getAppInfo(): { data: { app: string; version: string } } {
+    return {
+      data: {
+        app: packageInfo.name,
+        version: packageInfo.version,
+      },
+    };
   }
 }
